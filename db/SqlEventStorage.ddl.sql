9b1803a7699a04584d64c3a45df242acfa48b1c2
James Hicks <jhicks0300@gmail.com>
2010-03-12 16:37:44 -0600

    added sql schema ddl script for the sql event store

---
+++ db/SqlEventStorage.ddl.sql
@@ -0,0 +1,66 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+PRINT N'Creating [dbo].[EventStore]'
+GO
+CREATE TABLE [dbo].[EventStore]
+(
+[StreamId] [uniqueidentifier] NOT NULL,
+[Sequence] [int] NOT NULL IDENTITY(1, 1),
+[EventData] [varbinary] (max) NOT NULL
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating primary key [PK_EventStore] on [dbo].[EventStore]'
+GO
+ALTER TABLE [dbo].[EventStore] ADD CONSTRAINT [PK_EventStore] PRIMARY KEY CLUSTERED  ([StreamId], [Sequence])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating [dbo].[SnapshotStore]'
+GO
+CREATE TABLE [dbo].[SnapshotStore]
+(
+[SourceId] [uniqueidentifier] NOT NULL,
+[SnapshotData] [varbinary] (max) NOT NULL,
+[timestamp] [timestamp] NOT NULL
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating primary key [PK_SnapshotStore] on [dbo].[SnapshotStore]'
+GO
+ALTER TABLE [dbo].[SnapshotStore] ADD CONSTRAINT [PK_SnapshotStore] PRIMARY KEY CLUSTERED  ([SourceId])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'The database update succeeded'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'The database update failed'
+GO
+DROP TABLE #tmpErrors
+GO
